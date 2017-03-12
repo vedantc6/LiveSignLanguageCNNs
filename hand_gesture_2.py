cf81e933b4d975518a124d653f98a2ffd916c18c
vedantc6 <vedantc6@gmail.com>
2017-03-13 02:55:43 +0530

    Added blur and threshold

--- hand_gesture_2.py
+++ hand_gesture_2.py
@@ -3,7 +3,11 @@ import numpy as np
 
 # Parameter Values
 bgSubThreshold = 50
-threshold = 60  # binary threshold
+threshold = 170  # binary threshold
+cap_region_x_begin = 0.55
+cap_region_y_end = 0.8
+blurValue = 41  # gaussian blur
+
 # Variables
 BgCaptured = 0
 
@@ -23,7 +27,7 @@ def removeBG(frame):
 camera = cv2.VideoCapture(0)
 camera.set(10,200)
 cv2.namedWindow('Trackbar')
-cv2.createTrackbar('trh1', 'Trackbar', threshold, 100, printThreshold)
+cv2.createTrackbar('trh1', 'Trackbar', threshold, 255, printThreshold)
 
 
 while camera.isOpened():
@@ -31,7 +35,21 @@ while camera.isOpened():
     threshold = cv2.getTrackbarPos('trh1', 'Trackbar')
     frame = cv2.bilateralFilter(frame, 5, 50, 100)  # smoothing filter
     frame = cv2.flip(frame, 1)
+    cv2.rectangle(frame, (int(cap_region_x_begin*frame.shape[1]), 0),
+                    (frame.shape[1], int(cap_region_y_end*frame.shape[0])),
+                    (255, 0, 0), 2)
     cv2.imshow("Input", frame)
+    if BgCaptured == 1:
+        img = removeBG(frame)
+        img = img[0:int(cap_region_y_end*frame.shape[0]),
+                int(cap_region_x_begin*frame.shape[1]):frame.shape[1]]  # clip ROI
+        cv2.imshow('Mask', img)
+        # convert the image into binary image
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        blur = cv2.GaussianBlur(gray, (blurValue, blurValue), 0)
+        cv2.imshow('Blur', blur)
+        ret, thresh = cv2.threshold(blur, threshold, 255, cv2.THRESH_BINARY)
+        cv2.imshow('Threshold', thresh)
     k = cv2.waitKey(10)
     if k == 27:     # esc button
         break
