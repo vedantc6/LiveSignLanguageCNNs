4cf44d2298213b0d4f3dd6e1acdeb4976d22f191
vedantc6 <vedantc6@gmail.com>
2017-04-04 23:54:11 +0530

    Added convex hull.

---
+++ data_greyscale.py
@@ -0,0 +1,25 @@
+import os
+from PIL import Image
+from resizeimage import resizeimage
+
+database_dir = './asl_dataset'
+new_dir = './final_dataset/'
+
+
+def getImage(name, file, filename):
+    im = Image.open(file).convert('LA')
+    cover = resizeimage.resize_width(im, 256)
+    # print(cover.size)
+    new_dir_name = new_dir + name
+    if not os.path.exists(new_dir_name):
+        os.makedirs(new_dir_name)
+    cover.save(new_dir_name + '/' + filename[:-4] + '.png')
+    return name, cover, filename
+
+for old_dir_name in os.walk(database_dir):
+    folder = old_dir_name[0]
+    # print(folder)
+    for filename in os.listdir(folder):
+        if len(filename) > 1:
+            print(filename)
+            img = getImage(folder[-1], folder + '/' + filename, filename)
